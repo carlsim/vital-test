52dd5db2e03534fb4ba95f87de0d030e2cacd826
Carl Sim <similatron@gmail.com>
2021-02-10 15:27:22 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+/* eslint-env browser */
+(function() {
+  'use strict';
+  
+  // My JS
+  console.log('[Running JS] Hello World!');
+
+  //Variable declarations
+  const API_URL = '../data/data.json';
+
+  //logging output
+  // console.log(API_URL);
+
+  /**
+   * Get data from API
+   */
+  async function getData() {
+    // Variable declarations for data expected
+    const response = await fetch(`${API_URL}`);
+    const data = await response.json();
+
+    const countryCode = data.countryCode.toLowerCase();
+    const currentURL = document.location.href;
+
+    /**
+     * Check to see if URL contains the country code
+     * If doesnt exist, put it in URL otherwise don't do anything
+     */
+    if(currentURL.includes("?ca")) {
+    }else {
+      document.location.href += '?' + countryCode;
+    }
+  }
+
+  // Call async function to get the data from JSON
+  getData()
+  .catch(error => {
+    console.log(`ErrorL ${error}`);
+  });
+})();
